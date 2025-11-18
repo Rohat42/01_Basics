25a88640aa3c1f63e4f37ca5801e85ed6fb39f42
Rohat Yilmaz <rohatyilmaz42@icloud.com>
2025-11-18 16:19:14 +0100

    Übung

---
+++ 10/42.Passwort.js
@@ -0,0 +1,8 @@
+let input = "abc123";
+let password = "abc123";
+
+if (input==password){
+console.log("Access granted");
+}else{
+console.log("Accces denied");
+}
---
+++ 10/43.Number check.js
@@ -0,0 +1,21 @@
+let number = -3;
+
+
+if (number>0) {
+console.log("The number is positive");
+}else{
+console.log("The number is not positive")
+}
+
+
+if (number < 0) {
+console.log("The number is negative");
+} else {
+ console.log("The number is positive or 0");
+}
+
+if (number== 0 ){
+console.log("The number is zero");
+}else{
+  console.log("The number is not 0");
+}
