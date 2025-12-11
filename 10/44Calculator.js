2e3a9eae754fe08deaf1358cba8442e9761b10c9
Rohat Yilmaz <rohatyilmaz42@icloud.com>
2025-12-11 11:40:53 +0100

    Aufagebe

---
+++ 10/44Calculator.js
@@ -0,0 +1,9 @@
+let zahl1 = Number (prompt("erste zahl eingeben: "));
+let zahl2 = Number (prompt("zweite zahl eingeben: "));
+let art = prompt ("Rechenart (+ - * /): ");
+
+
+let erg;
+
+
+console.log();
---
+++ 10/47_Wetter.js
@@ -0,0 +1,17 @@
+let temp = 30;
+if (temp < 0) {
+console.log("Drinnen bleiben");
+} 
+
+else if (temp < 10) {
+console.log("Warm anziehen");
+}else if (temp < 25) {
+
+ console.log("Raus gehen");
+} else if (temp < 35) {
+
+console.log("Baden gehen");
+
+} else {
+console.log("In die Kühlbox gehen");
+}
