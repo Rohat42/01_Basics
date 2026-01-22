6a9d6087fdd01451585066d9f0a344853f2c1534
rohat <rohat@school.at>
2026-01-22 12:44:10 +0100

    Aufgabe 1 Arrays done

---
+++ 70_Arrays/Aufgaben.js/Aufgabe1.js
@@ -0,0 +1,15 @@
+let zahlen = []
+for (let i = 0; i < 7 ; i++) {
+zahlen.push(Math.floor(Math.random() * 10));
+
+}
+
+let summe = 0;
+for (let i = 0; i < zahlen.length; i++) { 
+    summe += zahlen[i];
+}
+
+console.log(summe);
+
+
+
