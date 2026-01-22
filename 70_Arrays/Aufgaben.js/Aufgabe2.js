135cd48276d58b0473bb441f990233046dba1e67
rohat <rohat@school.at>
2026-01-22 13:04:49 +0100

    2 Aufgabe arrays done

---
+++ 70_Arrays/Aufgaben.js/Aufgabe2.js
@@ -0,0 +1,9 @@
+let zahlen = [4,6, 10 , 0];
+let summe = 0; 
+
+for (let i = 0;  i < zahlen.lengtht; i++) {
+  summe = summe + zahlen[i];
+} 
+let average = summe/ zahlen.length;
+console.log(average); 
+
